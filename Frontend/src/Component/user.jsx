118fcd3aac7f5602d470f611dfc5c332b080df22
Naitik Raj <naitikraj10001@gmail.com>
2024-07-23 12:40:01 +0530

    Search fix

--- Frontend/src/Component/user.jsx
+++ Frontend/src/Component/user.jsx
@@ -6,6 +6,7 @@ const UserComponent = () => {
   const isDarkMode = useSelector((state) => state.information.isDarkMode);
   const user = useSelector((state) => state.information.user);
   const [friends, setFriends] = useState([]);
+  const [Searchfriend, setSearchfriend] = useState([]);
 
   const inputColor = isDarkMode ? "text-gray-300" : "text-gray-800";
   const buttonBgColor = isDarkMode ? "bg-blue-500" : "bg-blue-400";
@@ -42,11 +43,34 @@ const UserComponent = () => {
 
       const data = await response.json();
       setFriends(data);
+      setSearchfriend(data);
     } catch (error) {
       console.error("Error fetching friends:", error);
     }
   };
 
+  function handleSearch(e) {
+    let searchtext = e.target.value;
+    console.log("Searching Name ", searchtext);
+
+    if(searchtext === " "){
+      return;
+    }
+
+    // convert search text to lowercase
+    searchtext = searchtext.toLowerCase();
+
+    searchtext=searchtext.trim();
+
+    // filter friends based on search text such that if any friend name contains search text then it will be shown
+    const filteredFriends = Searchfriend.filter((friend) =>
+      friend.name.toLowerCase().includes(searchtext)
+    );
+
+    setFriends(filteredFriends);
+
+
+  }
   useEffect(() => {
     if (user) {
       console.log("Fetching friends... Frontend/src/Component/user.jsx:134");
@@ -70,28 +94,30 @@ const UserComponent = () => {
         >
           <input
             type="text"
-            placeholder="Search by user name..."
+            placeholder="Search by name..."
+            onChange={handleSearch}
             className={`px-2 py-1 w-full focus:outline-none ${inputColor} ${textColor} ${bgColor} border ${borderColor} rounded-md transition-colors duration-300`}
           />
-          <button
+          {/* <button
             className={`${textColor} p-2 rounded-full transition-colors duration-300 hover:${accentColor}`}
+          > */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+          {/* </button> */}
         </div>
+
         <div className={`h-full overflow-y-auto`}>
           {/* User List */}
           <div className={`${bgColor} p-4`}>
