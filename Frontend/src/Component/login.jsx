9f46f9bd9498905819dd260df73123458813189f
Naitik Raj <naitikraj10001@gmail.com>
2024-10-07 14:21:51 +0530

    Update

--- Frontend/src/Component/login.jsx
+++ Frontend/src/Component/login.jsx
@@ -41,7 +41,7 @@ function Login() {
   function handleSubmit(event) {
     event.preventDefault();
 
-    
+    // Login button
 
     const form = new FormData(event.target);
     const email = form.get("email");
